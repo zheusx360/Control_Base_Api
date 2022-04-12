b65f6d11f106e4197908e475d47caf3e09f6ea24
Rodrigo <rodrigo@kyte.com.br>
2022-04-12 02:20:04 -0300

    Remoção de console.log da verificação de tokens

--- helpers/verify-jwt.js
+++ helpers/verify-jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const verifyJwt = async (req, res, next) => {
-   console.log(token)
+   const token = req.headers['x-access-token'];
    jwt.verify(token,process.env.SECRET, (err, decoded) =>{
       if(err) return res.status(401).json({message: "Acesso negado! tente fazer o login novamente."});
       req.userId = decoded.userId;
