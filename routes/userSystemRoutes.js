f17166f7843a2c7d9e1e63c30fdf10c267a52f47
Rodrigo <rodrigo@kyte.com.br>
2022-05-30 12:13:32 -0300

    envio de email para ttc via api

--- routes/userSystemRoutes.js
+++ routes/userSystemRoutes.js
@@ -308,5 +308,30 @@ router.post('/verifyCode', async(req, res) =>{
 
 })
 
+
+//Rota para envio de emial TTEC
+router.post('/ttecSend', async (req,res) => {
+
+   let transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      secure: true,
+      auth: {
+        user: 'ttec.orcamento@gmail.com',
+        pass: process.env.TTEC_PASSWORD
+      }
+    });
+  
+    await transporter.sendMail({
+      from: 'ttec.orcamento@gmail.com',
+      to: 'ttec.orcamento@gmail.com',
+      subject: 'Nova solicitação de orçamento',
+      text: '',
+      html: `<h3>Nome: ${req.body.nome}</h3></br><h3>Telefone: ${req.body.fone}</h3></br><h3>Email: ${req.body.email}</h3></br><h3>${req.body.data}</h3></br><h3>Mensagem: </h3></br><h3>${req.body.text}</h3>`
+    })
+      .then(res.status(200).json({ message: "Email enviado com sucesso!" }))
+      .catch(error => res.status(500).json({ message: `Erro ao enviar o email ${error}` }));
+})
+
+
 module.exports = router
 
